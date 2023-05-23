6b69a52ba68d280cea00591d3aeb7a5ac986ad77
ManausK <1905246@kiit.ac.in>
2023-05-23 23:25:53 +0530

    react-chat-application

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { ChatEngine } from "react-chat-engine";
+import "./App.css";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChatEngine
+      height="100vh"
+      projectID="d0b80f55-55cc-43cb-8e46-8cd5f943c0a2"
+      userName="manausk"
+      userSecret="123456"
+    />
   );
-}
+};
 
 export default App;
